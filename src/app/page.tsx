deffe66bcea775f33c9b049a6633f166a7eafca8
Krishna Tiwari <kisnatwari@gmail.com>
2023-09-23 08:31:09 +0545

    Metadata updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import HeroSection from '@/components/Atoms/homepage/HeroSection'
 export default function Home() {
   return (
     <div className='flex-grow h-full flex'>
+      <head>
+        <link rel="icon" href="/favicon.jpeg" sizes="any" />
+      </head>
       <HeroSection />
     </div>
   )
