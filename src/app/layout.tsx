b3bc7b72b7fd894d5435d304ce472ac7e307d217
Krishna Tiwari <kisnatwari@gmail.com>
2023-10-05 21:29:34 +0545

    Globalizing small background images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
@@ -26,14 +27,23 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
           enableSystem
           disableTransitionOnChange
         >
-          <main className='h-screen overflow-auto flex flex-col'>
+          <main className='h-screen overflow-auto flex flex-col backdrop-blur-sm'>
             <NextTopLoader showSpinner={false} />
             <UnderConstructionMessage />
             <Navbar />
             <Suspense fallback={<Loading />}>
               {children}
+              <div className='absolute top-0 left-0 w-full h-full -z-10 overflow-hidden blur-sm'>
+                <img src="https://static-00.iconduck.com/assets.00/nextjs-icon-512x512-y563b8iq.png" alt="" className='w-20 absolute opacity-20 top-80 left-56' />
+                <img src="https://static-00.iconduck.com/assets.00/react-icon-512x456-5xl7nmtw.png" alt="" className='w-24 absolute opacity-10 bottom-16 left-96' />
+                <img src="https://static-00.iconduck.com/assets.00/laravel-icon-1990x2048-xawylrh0.png" alt="" className='w-20 absolute opacity-10 top-16 left-96' />
+                <img src="https://icons.veryicon.com/png/o/business/vscode-program-item-icon/typescript-def.png" alt="" className='w-20 absolute opacity-10 top-16 left-96 mt-40 ms-52' />
+                <span className="text-7xl top-80 left-72 absolute ms-96 mt-44 opacity-10">
+                  {`</>`}
+                </span>
+              </div>
             </Suspense>
-            <SocialLinks/>
+            <SocialLinks />
           </main>
         </ThemeProvider>
       </body>
--- src/components/Atoms/homepage/HeroSection.tsx
+++ src/components/Atoms/homepage/HeroSection.tsx
@@ -31,8 +31,8 @@ const fira_code = Fira_Code({
 const HeroSection = (props: Props) => {
 
     return (
-        <div className={`container relative ${saira.className}`}>
-            <div className='grid grid-cols-2 h-full backdrop-blur-sm'>
+        <div className={`container ${saira.className}`}>
+            <div className='grid grid-cols-2 h-full'>
                 <div className='h-full flex flex-col gap-5 justify-center'>
                     <div className='flex flex-col gap-2 justify-center'>
                         <p className='text-2xl text-yellow-600 dark:text-yellow-300'>Hi, I&apos;m Krishna Tiwari</p>
@@ -63,15 +63,6 @@ const HeroSection = (props: Props) => {
                         {<HeroImage />}
                     </div>
                 </div>
-            </div>
-            <div className='absolute top-0 left-0 w-full h-full -z-10 overflow-hidden'>
-                <img src="https://static-00.iconduck.com/assets.00/nextjs-icon-512x512-y563b8iq.png" alt="" className='w-20 absolute opacity-20 top-80 left-56' />
-                <img src="https://static-00.iconduck.com/assets.00/react-icon-512x456-5xl7nmtw.png" alt="" className='w-24 absolute opacity-10 bottom-16 left-96' />
-                <img src="https://static-00.iconduck.com/assets.00/laravel-icon-1990x2048-xawylrh0.png" alt="" className='w-20 absolute opacity-10 top-16 left-96' />
-                <img src="https://icons.veryicon.com/png/o/business/vscode-program-item-icon/typescript-def.png" alt="" className='w-20 absolute opacity-10 top-16 left-96 mt-40 ms-52' />
-                <span className="text-7xl top-80 left-72 absolute ms-96 mt-44 opacity-10">
-                    {`</>`}
-                </span>
             </div>
         </div >
     )
